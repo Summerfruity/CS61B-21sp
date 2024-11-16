98485a92590c1e86e87f8bf7b050b0af9229cdeb
Summer <2781455007@qq.com>
2024-11-16 21:12:12 +0800

    lab7 第二次

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -3,12 +3,11 @@ package bstmap;
 import java.util.Iterator;
 import java.util.Set;
 
-public class BSTMap<K extends Comparable<K>,V> implements Map61B<K,V>{
-
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     private BSTNode root;
 
-    /** Nested class*/
-    private class BSTNode{
+    /** Nested class */
+    private class BSTNode {
         private K key;
         private V value;
         private BSTNode left;
@@ -16,7 +15,7 @@ public class BSTMap<K extends Comparable<K>,V> implements Map61B<K,V>{
         private int size;
 
         /** Node constructor */
-        public BSTNode(K key, V value){
+        public BSTNode(K key, V value) {
             this.value = value;
             this.key = key;
             this.size = 1;
@@ -25,13 +24,13 @@ public class BSTMap<K extends Comparable<K>,V> implements Map61B<K,V>{
 
     /** Removes all the mappings from this map. */
     @Override
-    public void clear(){
+    public void clear() {
         root = null;
     }
 
     /** Returns true if this map contains a mapping for the specified key. */
     @Override
-    public boolean containsKey(K key){
+    public boolean containsKey(K key) {
         return get(key) != null;
     }
 
@@ -39,106 +38,103 @@ public class BSTMap<K extends Comparable<K>,V> implements Map61B<K,V>{
      * map contains no mapping for the key.
      */
     @Override
-    public V get(K key){
+    public V get(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("Key cannot be null.");
+        }
         return get(root, key);
     }
 
-    /** helper for get */
-    private V get(BSTNode node, K key){
-        if (node == null){
+    /** Helper for get */
+    private V get(BSTNode node, K key) {
+        if (node == null) {
             return null;
         }
         int cmp = key.compareTo(node.key);
-        if (cmp < 0){
+        if (cmp < 0) {
             return get(node.left, key);
-        }
-        else if(cmp == 0){
+        } else if (cmp == 0) {
             return node.value;
-        }
-        else {
+        } else {
             return get(node.right, key);
         }
     }
 
     /** Returns the number of key-value mappings in this map. */
     @Override
-    public int size(){
-        return root.size;
+    public int size() {
+        return size(root);
+    }
+
+    private int size(BSTNode n) {
+        if (n == null) {
+            return 0;
+        }
+        return n.size;
     }
 
     /** Associates the specified value with the specified key in this map. */
     @Override
-    public void put(K key, V value){
-        put(root, key, value);
+    public void put(K key, V value) {
+        if (key == null) {
+            throw new IllegalArgumentException("Key cannot be null.");
+        }
+        root = put(root, key, value);
     }
 
-    /** helper for put */
-    private void put(BSTNode n, K key, V value){
+    /** Helper for put */
+    private BSTNode put(BSTNode n, K key, V value) {
+        if (n == null) {
+            return new BSTNode(key, value);
+        }
         int cmp = key.compareTo(n.key);
-        if (cmp < 0){
-            if (n.left == null){
-                n.left = new BSTNode(key, value);
-            }
-            else {
-                put(n.left, key, value);
-            }
+        if (cmp < 0) {
+            n.left = put(n.left, key, value);
+        } else if (cmp > 0) {
+            n.right = put(n.right, key, value);
+        } else {
+            n.value = value; // Overwrite value if key exists
         }
-        else if(cmp == 0){
-            return;
-        }
-        else {
-            if (n.right == null){
-                n.right = new BSTNode(key, value);
-            }
-            else {
-                put(n.right, key, value);
-            }
-        }
-
+        n.size = size(n.left) + size(n.right) + 1;
+        return n;
     }
 
-    /** Returns a Set view of the keys contained in this map. Not required for Lab 7.
-     * If you don't implement this, throw an UnsupportedOperationException. */
+    /** Returns a Set view of the keys contained in this map. */
     @Override
-    public Set<K> keySet(){
+    public Set<K> keySet() {
         throw new UnsupportedOperationException("Unsupported operation!");
     }
 
-    /** Removes the mapping for the specified key from this map if present.
-     * Not required for Lab 7. If you don't implement this, throw an
-     * UnsupportedOperationException. */
+    /** Removes the mapping for the specified key from this map if present. */
     @Override
-    public V remove(K key){
+    public V remove(K key) {
         throw new UnsupportedOperationException("Unsupported operation!");
     }
 
     /** Removes the entry for the specified key only if it is currently mapped to
-     * the specified value. Not required for Lab 7. If you don't implement this,
-     * throw an UnsupportedOperationException.*/
+     * the specified value.
+     */
     @Override
-    public V remove(K key, V value){
+    public V remove(K key, V value) {
         throw new UnsupportedOperationException("Unsupported operation!");
     }
 
     @Override
-    public Iterator<K> iterator(){
+    public Iterator<K> iterator() {
         throw new UnsupportedOperationException("Unsupported operation!");
     }
 
-    public void printInOrder(){
+    public void printInOrder() {
         printNode(root);
     }
 
-    /** helper for print */
-    private void printNode(BSTNode n){
-        if (n==null){
+    /** Helper for print */
+    private void printNode(BSTNode n) {
+        if (n == null) {
             return;
         }
-        else {
-            printNode(n.left);
-            System.out.print(n.key + "," + n.value);
-            System.out.print(" ");
-            printNode(n.right);
-        }
+        printNode(n.left);
+        System.out.println(n.key + " : " + n.value);
+        printNode(n.right);
     }
 }
